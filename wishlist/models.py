a8c62586dfba4dcb3c519ed0d73011b59319a540
syifa.afra <syifa.afra@ui.ac.id>
2022-09-07 22:23:10 +0700

    Finish Lab 1

---
+++ wishlist/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+from django.db import models
+
+class BarangWishlist(models.Model):
+    nama_barang = models.CharField(max_length=50)
+    harga_barang = models.IntegerField()
+    deskripsi = models.TextField()
